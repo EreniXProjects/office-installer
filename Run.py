13e58c645df11a002c419704cec8aa84c63aa0a0
EreniX <43515826+EreniXProjects@users.noreply.github.com>
2021-11-22 19:46:41 +0300

    Update Run.py

---
+++ Run.py
@@ -0,0 +1,17 @@
+import os
+
+print("====================\n1 - Install Office\n====================\n0 - Exit Installer\n====================\n\n====================")
+Case = int(input("Case: "))
+print("====================")
+
+class Exit():
+ os.system("exit")
+
+class Install():
+ os.system("start .\sources\Install.py")
+
+if(Case == 1):
+ Install();
+
+if(Case == 0):
+ Exit();
