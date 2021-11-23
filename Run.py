6f0f16b08cb1e328c4b4b630fc7566c898a576a8
EreniX <43515826+EreniXProjects@users.noreply.github.com>
2021-11-23 18:23:54 +0300

    Add files via upload

--- Run.py
+++ Run.py
@@ -1,7 +1,7 @@
 import os
 
-print("====================\n1 - Install Office\n====================\n0 - Exit Installer\n====================\n\n====================")
-Case = int(input("Case: "))
+print("|====================|\n| 1 - Install Office |\n|====================|\n| 0 - Exit Installer |\n|====================|\n|====================|")
+Case = int(input("| Case: "))
 print("====================")
 
 class Exit():
@@ -15,5 +15,3 @@ if(Case == 1):
 
 if(Case == 0):
  Exit();
-
- # https://filecr.com/windows/microsoft-office-2021-professional-plus
--- sources/Install.py
+++ sources/Install.py
@@ -1,13 +1,13 @@
 import os
 
-print("======================================\nEnter Language ID and start INSTALL!\n======================================\nExample: Example: en-US or en-us\n======================================")
+print("|======================================|\n| Enter Language ID and start INSTALL! |\n|======================================|\n| Example: Example: en-US or en-us     |\n|======================================|")
 lang = input("Lang: ")
 
 langFile = "././languages/"+lang+".xml"
 
 if os.path.isfile(langFile) and os.access(langFile, os.R_OK):
-    print("Office Installing with",lang, "Language...")
-    os.system("start ./sources/setup.exe /configure "+langFile)
+ print("Office Installing with",lang, "Language...")
+ os.system("start ./sources/setup.exe /configure "+langFile)
 
 else:
-    print(lang, "Named Language NOT FOUND!!")
+ print(lang, "Named Language 'NOT FOUND!!' ")
